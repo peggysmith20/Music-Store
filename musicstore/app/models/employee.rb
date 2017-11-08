dfa8b5e780be7b09d3d5dfba7289c49b0e1c53a2
amazing-username <kdeng02@aurora.edu>
2017-11-08 10:04:40 -0600

    Error when employee is created no longer comes up. A new error occurs. The new error will not create a character because it states that the employee does not exist

--- musicstore/app/models/employee.rb
+++ musicstore/app/models/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ApplicationRecord
   belongs_to :employee
+  validates :employee_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :job_title, presence: true
+  validates :pay, presence: true
+  validates :phone, presence: true
+  validates :schedule_slot, presence: true
+  self.primary_key = "employee_id"
+  self.primary_key = "schedule_slot"
 end
